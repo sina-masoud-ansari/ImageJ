02b456664d034b5080cc93b751d8ea05716f93de
smas036 <sina.masoud.ansari@gmail.com>
2012-04-17 00:09:20 +1200

    param tests

--- src/test/ij/plugin/filter/FiltersTest.java
+++ src/test/ij/plugin/filter/FiltersTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -11,6 +12,7 @@ import java.util.Collection;
 import java.net.URL;
 
 import org.apache.commons.math3.stat.inference.TTest;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -19,18 +21,45 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(value = Parameterized.class)
 public class FiltersTest {
 
-	private final static double RANGE = 25;
-	private final static double ALPHA = 0.05;
+	private final static int
 	
-	private final ImagePlus imgA, imgB;
-	private final int nChannels;
+		// Parallelisation approaches
+		P_NONE = ImageProcessor.P_NONE,
+		P_SERIAL = ImageProcessor.P_SERIAL,
+		P_SIMPLE = ImageProcessor.P_SIMPLE,
+		
+		//Number of channels per image type
+		CH_COLOR_RGB = 3,
+		CH_COLOR_256 = 3,
+		CH_GRAY8 = 1,
+		CH_GRAY16 = 1,
+		CH_GRAY32 = 1;
+	
+	private final static double
+	
+		// Statistical parameters
+		RANGE = 25,
+		ALPHA = 0.05;
 
+	private final static String 
+	
+		// Example images of each image type
+		COLOR_RGB = "/resources/images/COLOR_RGB.jpg",
+		COLOR_256 = "/resources/images/COLOR_256.jpg",
+		GRAY8 = "/resources/images/GRAY8.jpg",
+		GRAY16 = "/resources/images/GRAY8.jpg",
+		GRAY32 = "/resources/images/GRAY8.jpg";
+	
+	private final static DecimalFormat DP3 = new DecimalFormat("#.###");
+	
+	private ImagePlus imgA, imgB;
+	private final int nChannels, mode;
+	private final URL url;
 
-	public FiltersTest(String s, int n) {	
-		URL url = this.getClass().getResource(s);
-		imgA = new ImagePlus(url.getPath());
-		imgB = new ImagePlus(url.getPath());
+	public FiltersTest(String s, int n, int m) {	
+		url = this.getClass().getResource(s);
 		nChannels = n;
+		mode = m;
 	}
 
 	//TODO: javadoc
@@ -43,32 +72,41 @@ public class FiltersTest {
 	public static Collection<Object[]> testImages() {
 		
 		Object[][] images = new Object[][] { 
-				{ "/resources/images/COLOR_RGB.jpg", 3 }, 
-				{ "/resources/images/COLOR_256.jpg", 3 }, 
-				{ "/resources/images/GRAY8.jpg", 1 },
-				{ "/resources/images/GRAY16.jpg", 1 },
-				{ "/resources/images/GRAY32.jpg", 1 }
+				{ COLOR_RGB, CH_COLOR_RGB, P_NONE }, { COLOR_RGB, CH_COLOR_RGB, P_SERIAL }, { COLOR_RGB, CH_COLOR_RGB, P_SIMPLE },
+				{ COLOR_256, CH_COLOR_256, P_NONE }, { COLOR_256, CH_COLOR_256, P_SERIAL }, { COLOR_256, CH_COLOR_256, P_SIMPLE },
+				{ GRAY8, CH_GRAY8, P_NONE }, { GRAY8, CH_GRAY8, P_SERIAL }, { GRAY8, CH_GRAY8, P_SIMPLE },
+				{ GRAY16, CH_GRAY16 , P_NONE }, { GRAY16, CH_GRAY16 , P_SERIAL }, { GRAY16, CH_GRAY16 , P_SIMPLE },
+				{ GRAY32, CH_GRAY32, P_NONE }, { GRAY32, CH_GRAY32, P_SERIAL }, { GRAY32, CH_GRAY32, P_SIMPLE }
 		};
 		return Arrays.asList(images);
 	}
+	
+	@Before
+	public void setUp(){
+		imgA = new ImagePlus(url.getPath());
+		imgB = new ImagePlus(url.getPath());		
+	}
 
 	@Test
-	public void testAddNoiseSERIAL() {
+	public void testAddNoise() {
 		
-		System.out.println("Image: "+imgA.getTitle());
+		System.out.println("Image: "+imgA.getTitle()+", mode: "+mode);
 		
 		imgA.getProcessor().noise(RANGE, ImageProcessor.P_NONE);
-		imgB.getProcessor().noise(RANGE, ImageProcessor.P_SERIAL);
+		imgB.getProcessor().noise(RANGE, mode);
 		
 		ArrayList<double[]> aChannles = getChannels(imgA, nChannels);
 		ArrayList<double[]> bChannles = getChannels(imgB, nChannels);
 		
 		TTest test = new TTest();
 		boolean reject;
-		for (int i = 0; i < nChannels; i++){	
-			//double pValue = test.pairedTTest(aChannles.get(i), bChannles.get(i));
-			//System.out.println("Channel: "+i+", p-value: "+pValue);
-			reject = test.pairedTTest(aChannles.get(i), bChannles.get(i), ALPHA);
+		for (int i = 0; i < nChannels; i++){
+			double pValue = test.homoscedasticTTest(aChannles.get(i), bChannles.get(i));
+			System.out.println("Channel: "+i+", p-value: "+Double.valueOf(DP3.format(pValue))+", alpha: "+ALPHA);
+			reject = test.homoscedasticTTest(aChannles.get(i), bChannles.get(i), ALPHA);
+			if (reject){			
+				System.out.println("\tRejected with confidence: "+(1 - ALPHA));			
+			}
 			assertEquals(false, reject);
 		}
 	}
