8f066f3eddba6ed893c016260c56ecbbb751e5ed
unknown <Rucha@Rucha-PC.(none)>
2012-04-21 14:23:53 +1200

    added parallel shadows

--- src/test/ij/parallel/ParallelImagePlusTest.java
+++ src/test/ij/parallel/ParallelImagePlusTest.java
@@ -1,5 +1,15 @@
 package ij.parallel;
 
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
 public class ParallelImagePlusTest {
 
+	@Test
+	public void testNothing(){
+		assertEquals(1,1);
+	}
+	
+	
 }
