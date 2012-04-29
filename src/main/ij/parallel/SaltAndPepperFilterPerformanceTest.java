1aca7d18cc9c2addb3d3482a0fbf88e73cc9fd85
Arvie Carpio <acar149@aucklanduni.ac.nz>
2012-04-30 08:29:01 +1200

    edited salt and pepper performance test

--- src/main/ij/parallel/SaltAndPepperFilterPerformanceTest.java
+++ src/main/ij/parallel/SaltAndPepperFilterPerformanceTest.java
@@ -1,30 +1,42 @@
 package ij.parallel;
 
+import ij.parallel.plugin.filter.ParallelSaltAndPepper;
+import ij.plugin.filter.PlugInFilterRunner;
+import ij.plugin.filter.SaltAndPepper;
 import ij.process.ImageProcessor;
 
 public class SaltAndPepperFilterPerformanceTest extends PerformanceTest {
 
-	public SaltAndPepperFilterPerformanceTest(String n, String p) {
-		super(n, p);
+	private static final double PERCENT = 0.05;
+	
+	public SaltAndPepperFilterPerformanceTest(String path) {
+		super("Salt and Pepper ORIGINAL", path);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void run_P_NONE(ImageProcessor ip) {
 		// TODO Auto-generated method stub
-		
+		SaltAndPepper sp = new SaltAndPepper();
+		sp.setup("salt_and_pepper", null);
+		sp.run(ip);
 	}
 
 	@Override
 	public void run_P_SERIAL(ImageProcessor ip) {
 		// TODO Auto-generated method stub
-		
+		ParallelSaltAndPepper parallel_sp = new ParallelSaltAndPepper();
+		parallel_sp.setup("salt_and_pepper serial", null);
+		parallel_sp.run(ip);
+
 	}
 
 	@Override
 	public void run_P_SIMPLE(ImageProcessor ip) {
 		// TODO Auto-generated method stub
-		
+		ParallelSaltAndPepper parallel_sp = new ParallelSaltAndPepper();
+		parallel_sp.setup("salt_and_pepper simple", null);
+		parallel_sp.run(ip);
 	}
 
 }
