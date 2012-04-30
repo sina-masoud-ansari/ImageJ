53096a982e291620d90184cfe7fb45e475467522
Arvie Carpio <acar149@aucklanduni.ac.nz>
2012-04-30 14:19:37 +1200

    making performance.bat work. as of now there's trouble in trying to run the SampleImageCreator.java due to some jar files which might not be properly located. will fix this asap.

--- performance.bat
+++ performance.bat
@@ -2,13 +2,12 @@ set BIN_DIR=%1
 set RESULTS_DIR=%2
 set MAX_MEM=%3
 set MACRO_DIR=%BIN_DIR%/macros
-set SAMPLE_DIR=build/resources/images/sample
+set SAMPLE_DIR=/build/resources/images/sample
 
-IF NOT EXIST %SAMPLE_DIR% GOTO MAKE_DIR
+IF NOT EXIST %SAMPLE_DIR% mkdir %SAMPLE_DIR%
 
-:MAKE_DIR
-	mkdir %SAMPLE_DIR%
+FOR /L %%A IN (1,1,10) DO CALL create_image.bat %%A %BIN_DIR% %MAX_MEM% %SAMPLE_DIR% 
 
-FOR /L %%A IN (1,1,10) DO CALL create_image.bat %%A %BIN_DIR% %MAX_MEM% %SAMPLE_DIR%
+FOR /F %%B IN (%SAMPLE_DIR%/*.tif) DO CALL perform_performance_test.bat %%B %BIN_DIR% %MAX_MEM% %RESULTS_DIR%
 
-FOR /F %%B IN (%SAMPLE_DIR%/) DO CALL perform_performance_test.bat %%B %BIN_DIR% %MAX_MEM% %RESULTS_DIR%
+
